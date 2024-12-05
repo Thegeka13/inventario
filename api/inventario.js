ed717066e31048b8f8b380b50d8294b41fd2676e
Thegeka13 <rafaelcamachohdz1303@gmail.com>
2024-12-04 20:40:51 -0600

    Correcciones

--- api/inventario.js
+++ api/inventario.js
@@ -4,7 +4,6 @@ const router = express.Router(); // Usamos router en vez de app
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Conexión con la base de datos
 const { connection } = require("../config.db");
 
 // Se Realiza la consulta de la base de datos de todos los productos existentes 
@@ -18,12 +17,10 @@ const getInventario = (request, response) => {
     });
 };
 
-// Ruta GET para inventario
 router.route("/inventario").get(getInventario);
 
-// Se realiza la consulta de la base de datos por ID de producto desde el cuerpo de la solicitud
 const obtenerInventarioPorId = (request, response) => {
-    const { producto_id: producto_id } = request.body; // Obtener el ID del producto desde el cuerpo de la solicitud
+    const { producto_id } = request.body; // Ajuste
     connection.query("SELECT * FROM inventario WHERE producto_id = ?", [producto_id], (error, results) => {
         if (error) {
             console.error("Error al consultar inventario:", error);
@@ -33,19 +30,15 @@ const obtenerInventarioPorId = (request, response) => {
     });
 };
 
-// Ruta POST para inventario por ID desde el cuerpo de la solicitud
 router.route("/inventario/id").post(obtenerInventarioPorId);
-
 
 const sumarInventario = (request, response) => {
     const { cantidad, producto_id } = request.body;
 
-    // Verificar si la cantidad es un número válido y mayor que 0
     if (!cantidad || isNaN(cantidad) || cantidad <= 0) {
         return response.status(400).json({ error: "Cantidad inválida" });
     }
 
-    // Consultamos el inventario actual del producto
     connection.query("SELECT cantidad FROM inventario WHERE producto_id = ?", [producto_id], (error, results) => {
         if (error) {
             console.error("Error al obtener el inventario:", error);
@@ -57,9 +50,8 @@ const sumarInventario = (request, response) => {
         }
 
         let cantidad_actual = results[0].cantidad;
-        let nueva_cantidad = cantidad_actual + cantidad; // Actualizamos la cantidad
+        let nueva_cantidad = cantidad_actual + cantidad;
 
-        // Actualizamos el inventario en la base de datos
         connection.query(
             "UPDATE inventario SET cantidad = ? WHERE producto_id = ?",
             [nueva_cantidad, producto_id],
@@ -75,18 +67,15 @@ const sumarInventario = (request, response) => {
     });
 };
 
-// Ruta POST para actualizar el inventario
 router.route("/inventario/sumar").post(sumarInventario);
 
 const restarInventario = (request, response) => {
     const { cantidad, producto_id } = request.body;
 
-    // Verificar si la cantidad es un número válido y mayor que 0
     if (!cantidad || isNaN(cantidad) || cantidad <= 0) {
         return response.status(400).json({ error: "Cantidad inválida" });
     }
 
-    // Consultamos el inventario actual del producto
     connection.query("SELECT cantidad FROM inventario WHERE producto_id = ?", [producto_id], (error, results) => {
         if (error) {
             console.error("Error al obtener el inventario:", error);
@@ -98,9 +87,8 @@ const restarInventario = (request, response) => {
         }
 
         let cantidad_actual = results[0].cantidad;
-        let nueva_cantidad = cantidad_actual - cantidad; // Actualizamos la cantidad
+        let nueva_cantidad = cantidad_actual - cantidad;
 
-        // Actualizamos el inventario en la base de datos
         connection.query(
             "UPDATE inventario SET cantidad = ? WHERE producto_id = ?",
             [nueva_cantidad, producto_id],
@@ -116,7 +104,6 @@ const restarInventario = (request, response) => {
     });
 };
 
-// Ruta POST para restar el inventario
 router.route("/inventario/restar").post(restarInventario);
 
-module.exports = router; // Exportamos el router
+module.exports = router;
