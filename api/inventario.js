6596c4780ade77dcb8eb375cff85e1c3b6085837
Thegeka13 <rafaelcamachohdz1303@gmail.com>
2024-12-04 19:14:43 -0600

    Correccion de La resta de productos y las busquedas individuales

--- api/inventario.js
+++ api/inventario.js
@@ -22,9 +22,9 @@ const getInventario = (request, response) => {
 router.route("/inventario").get(getInventario);
 
 // Se realiza la consulta de la base de datos por ID de producto desde el cuerpo de la solicitud
-const getInventarioById = (request, response) => {
-    const { id: productId } = request.body; // Obtener el ID del producto desde el cuerpo de la solicitud
-    connection.query("SELECT * FROM inventario WHERE id = ?", [productId], (error, results) => {
+const obtenerInventarioPorId = (request, response) => {
+    const { producto_id: producto_id } = request.body; // Obtener el ID del producto desde el cuerpo de la solicitud
+    connection.query("SELECT * FROM inventario WHERE producto_id = ?", [producto_id], (error, results) => {
         if (error) {
             console.error("Error al consultar inventario:", error);
             return response.status(500).json({ error: "Error al obtener inventario" });
@@ -34,11 +34,8 @@ const getInventarioById = (request, response) => {
 };
 
 // Ruta POST para inventario por ID desde el cuerpo de la solicitud
-router.route("/inventario/id").post(getInventarioById);
+router.route("/inventario/id").post(obtenerInventarioPorId);
 
-
-// Ruta GET para inventario
-router.route("/inventario/buscar").get(buscarInvetarioProducto);
 
 const sumarInventario = (request, response) => {
     const { cantidad, producto_id } = request.body;
@@ -79,7 +76,7 @@ const sumarInventario = (request, response) => {
 };
 
 // Ruta POST para actualizar el inventario
-router.route("/inventario/sumar").post(actualizarInventario);
+router.route("/inventario/sumar").post(sumarInventario);
 
 const restarInventario = (request, response) => {
     const { cantidad, producto_id } = request.body;
@@ -120,6 +117,6 @@ const restarInventario = (request, response) => {
 };
 
 // Ruta POST para restar el inventario
-router.route("/inventario/restar").post(actualizarInventario);
+router.route("/inventario/restar").post(restarInventario);
 
 module.exports = router; // Exportamos el router
