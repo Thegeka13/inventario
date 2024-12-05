503f5f87067f178ab78d643c23bf7775e6db617a
Thegeka13 <rafaelcamachohdz1303@gmail.com>
2024-12-04 22:38:40 -0600

    Modificaciones para vercel

--- index.js
+++ index.js
@@ -4,19 +4,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Cargar las rutas desde el archivo rutas/inventario.js
-const consultarInventario = require("./api/consultarInventario"); 
+// Cargar las rutas desde los archivos correspondientes
+const consultarInventario = require("./api/consultarInventarios"); 
 const restarInventario = require("./api/restarInventario"); 
 const sumarInventario = require("./api/sumarInventario");
-//const inventario = require('./api/inventario');
 
 app.use("/api", consultarInventario); 
 app.use("/api", restarInventario); 
 app.use("/api", sumarInventario);
-//app.use("/api", inventario);
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en el puerto 3300");
-});
-
+// Exportar la aplicación para Vercel
 module.exports = app;
